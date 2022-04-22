7321b03099aede94616b336487c0f479be357bf4
AliAbualsaud <ali-abualsaud@utexas.edu>
2022-04-22 15:19:34 -0500

    Create Sonar_Example.ino

---
+++ Energia/Sonar_Example/Sonar_Example.ino
@@ -0,0 +1,23 @@
+#define trigPin 12
+#define echoPin 13
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(trigPin,OUTPUT);
+  pinMode(echoPin,INPUT);
+}
+
+void loop() {
+int duration;
+float distance;
+int conversion;
+digitalWrite(trigPin,HIGH);
+delayMicroseconds(1000);
+digitalWrite(trigPin,LOW);
+duration = pulseIn(echoPin,HIGH);
+distance = (duration/2) / 74.07;
+
+Serial.println(distance);
+delay(10);
+  
+}
