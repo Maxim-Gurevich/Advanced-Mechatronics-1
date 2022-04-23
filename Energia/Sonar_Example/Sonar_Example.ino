cecfd43ac077f228ec6b396a371408dfe4d4fc92
Maxim-Gurevich <65980337+Maxim-Gurevich@users.noreply.github.com>
2022-04-23 12:22:10 -0500

    Update Sonar_Example.ino

--- Energia/Sonar_Example/Sonar_Example.ino
+++ Energia/Sonar_Example/Sonar_Example.ino
@@ -1,5 +1,5 @@
-#define trigPin 30
-#define echoPin 29
+#define trigPin 11
+#define echoPin 10
 
 void setup() {
   Serial.begin(9600);
@@ -18,10 +18,10 @@ delayMicroseconds(10);
 digitalWrite(trigPin,LOW);
 delayMicroseconds(2);
 duration = pulseIn(echoPin,HIGH,10000000UL);
-distance = (duration*100) / 5882;
-//Serial.println(distance);
-boolean boop=digitalRead(echoPin);
-Serial.println(boop);
-//delay(1000);
+distance = duration*100/5882;
+Serial.println(duration);
+//boolean boop=digitalRead(echoPin);
+//Serial.println(boop);
+delay(1000);
   
 }
