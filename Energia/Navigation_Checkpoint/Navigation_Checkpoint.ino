3ddec4205ed307c76294eab47fa90c23f9f2cb68
AliAbualsaud <ali-abualsaud@utexas.edu>
2022-04-29 08:48:19 -0500

    Update Navigation_Checkpoint.ino

--- Energia/Navigation_Checkpoint/Navigation_Checkpoint.ino
+++ Energia/Navigation_Checkpoint/Navigation_Checkpoint.ino
@@ -1,5 +1,8 @@
+
+// Libraries
 #include "Encoder.h"
 #include "SimpleRSLK.h"
+
 // Car line following Sensors
 uint16_t sensorVal[LS_NUM_SENSORS];
 uint16_t sensorMaxVal[LS_NUM_SENSORS];
@@ -10,31 +13,39 @@ uint16_t normalSpeedright = 13 ;
 uint16_t normalSpeedleft = 16.8 ;
 uint16_t sensorCalVal[LS_NUM_SENSORS];
 uint8_t lineColor = DARK_LINE;
-int Stop;
+uint32_t linePos;
+
+// Encoder Pins & Vars
 uint16_t ela_pin = P10_5;
 uint16_t elb_pin = P5_2;
 uint16_t era_pin = P10_4;
 uint16_t erb_pin = P5_0;
-int Shimmey = 0;
-int Shimmey2 = 0;
-int counter;
-int values[10] = {0, 0, 0, 0, 1, 0, 0, 0, 0, 1};
-int Beacon;
 int Left_En;
 int Right_En;
-uint32_t linePos = getLinePosition(sensorCalVal, lineColor);
-// First Sonar Sensor
+
+// Vars for when no beacons detected
+int Shimmey = 0;
+int Shimmey2 = 0;
+
+// Vars for when we can't turn
+int counter;
+int i;
+int const imax = 50; //Changes the size of the array
+int Values[imax];
+
+// Save which beacon was on
+int Beacon;
+
+// Right Sonar Sensor
 uint16_t TRIG1_PIN  = P9_1;
 uint16_t ECHO1_PIN  = P8_3;
-int Sonar1_Dist;
 int Right_Sonar;
 
-// Second Sonar Sensor
+// Left Sonar Sensor
 uint16_t TRIG2_PIN = P9_3;
 uint16_t ECHO2_PIN = P6_3;
-int Sonar2_Dist;
 int Left_Sonar;
-int Sonar_Diff;
+int Sonar_Diff; // right - left
 
 // IR Sensors
 int IRleft = 6;
@@ -56,8 +67,8 @@ states Current_State;
 
 void setup() {
 
-  // initialize serial communication at 9600 bits per second:
-  Serial.begin(115200); // msp430g2231 must use 4800
+  Serial.begin(115200);
+
   //  make the on - board pushbutton's pin an input pullup:
   pinMode(IRmid, INPUT_PULLUP); // NOTE: because this is a pullup, a 1 indicates no beacon detected, 0 is yes beacon detected
   attachInterrupt(digitalPinToInterrupt(IRmid), blink, CHANGE);
@@ -73,9 +84,15 @@ void setup() {
   Current_State = Adjust_Vertical;
   setupEncoder(ela_pin, elb_pin, era_pin, erb_pin);
 
+  // Creating the values array
+  for (i = 0; i < imax ; i++) {
+    Values[i] = 0;
+  }
+  Values[imax - 1] = 1;
+  Values[(imax / 2) - 1] = 1;
 }
 
-// Sonar Sensors
+// Sonar Sensors Function
 int Sonar_Sensors() {
   digitalWrite(TRIG1_PIN, LOW);
   delayMicroseconds(100);
@@ -100,47 +117,56 @@ int Sonar_Sensors() {
   delay(1);
 }
 
+void Not_Turning() {
+
+  
+}
+
 void blink() {
   state = !state;
 }
 
-
 void loop() {
-
 
   enableMotor(BOTH_MOTORS);
 
   switch (Current_State) {
     case Adjust_Vertical:
       Sonar_Sensors();
-      // When the robot is close to the backwall (can't turn), go backwards
       Right_En = 0;
       Left_En = 0;
-      // we are going to take the last 3 encoder ticks (more if needed) till we move forward
-      // first 3 are right encoder, last 3 are left
       resetLeftEncoderCnt();
       resetRightEncoderCnt();
+
+      // When the robot is close to the backwall (can't turn), go backwards
+      // we are going to take the last 3 encoder ticks (more if needed) till we move forward
+      // first 3 are right encoder, last 3 are left
 
       while (Left_Sonar > 45 || Right_Sonar > 45) {
         // Checks the first and last encoder values found, if they are the same,
         // the wheels are not turning
-        if (values[0] == values[4] || values[5] == values[9]) {
+        if (Values[0] == Values[(imax / 2) - 1] || Values[imax / 2] == Values[imax - 1]) {
           // if we cant turn we move backwards set distance 20 pulses (might need to change).
           setMotorDirection(BOTH_MOTORS, MOTOR_DIR_BACKWARD);
           setMotorSpeed(BOTH_MOTORS, normalSpeed);
           delay(500);
-          setMotorSpeed(BOTH_MOTORS,0);
+          setMotorSpeed(BOTH_MOTORS, 0);
           // This ensures the backup won't be called again (might not be needed)
-          values[0] = 0;
-          values[1] = 0;
-          values[2] = 0;
-          values[3] = 0;
-          values[4] = 1;
-          values[5] = 0;
-          values[6] = 0;
-          values[7] = 0;
-          values[8] = 0;
-          values[9] = 1;
+          Values[0] = 0;
+          Values[(imax / 2) - 1] = 1;
+          Values[imax / 2] = 0;
+          Values[imax - 1] = 1;
+
+          //          values[0] = 0;
+          //          values[1] = 0;
+          //          values[2] = 0;
+          //          values[3] = 0;
+          //          values[4] = 1;
+          //          values[5] = 0;
+          //          values[6] = 0;
+          //          values[7] = 0;
+          //          values[8] = 0;
+          //          values[9] = 1;
         }
         setMotorDirection(LEFT_MOTOR, MOTOR_DIR_FORWARD);
         setMotorDirection(RIGHT_MOTOR, MOTOR_DIR_BACKWARD);
@@ -148,20 +174,27 @@ void loop() {
         setMotorSpeed(RIGHT_MOTOR, normalSpeedright);
         Right_En = getEncoderRightCnt();
         Left_En = getEncoderLeftCnt();
+
         // This will update the list after finding the encoder counts
-        values[0] = values[1];
-        values[1] = values[2];
-        values[2] = values[3];
-        values[3] = values[4];
-        values[4] = Right_En;
-        values[5] = values[6];
-        values[6] = values[7];
-        values[7] = values[8];
-        values[8] = values[9];
-        values[9] = Left_En;
+        for (i = 1 ; i < imax ; i++) {
+          Values[i - 1] = Values[i];
+        }
+        Values[(imax / 2) - 1] = Right_En;
+        Values[imax - 1] = Left_En;
+
+        //        values[0] = values[1];
+        //        values[1] = values[2];
+        //        values[2] = values[3];
+        //        values[3] = values[4];
+        //        values[4] = Right_En;
+        //        values[5] = values[6];
+        //        values[6] = values[7];
+        //        values[7] = values[8];
+        //        values[8] = values[9];
+        //        values[9] = Left_En;
         Sonar_Sensors();
       }
-
+      setMotorSpeed(BOTH_MOTORS, 0);
       Right_En = 0;
       Left_En = 0;
       resetLeftEncoderCnt();
@@ -181,8 +214,7 @@ void loop() {
 
         Sonar_Sensors();
       }
-      setMotorSpeed(LEFT_MOTOR, 0);
-      setMotorSpeed(RIGHT_MOTOR, 0);
+      setMotorSpeed(BOTH_MOTORS, 0);
       Current_State = Adjust_Horizontal;
       break;
 
